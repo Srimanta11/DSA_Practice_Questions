630bc1e2d9c5db5b52873721f3b45d0f59df8bdf
Abhiraj Chatterjee <1905003@kiit.ac.in>
2021-10-21 21:29:21 +0530

    a solution to the path sum 3 problem 437 of leetcode

---
+++ LeetCode/437_Path_sum_III.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int c=0;
+    void solve(TreeNode* root,int k,int sum)
+    {
+        if(root)
+        {
+            sum+=root->val;
+            if(sum==k)
+            {
+                c++;
+            }
+            solve(root->left,k,sum);
+            solve(root->right,k,sum);
+        }
+    }
+    void checkNode(TreeNode* root,int k,int sum)
+    {
+        if(root)
+        {
+            checkNode(root->left,k,sum);
+            checkNode(root->right,k,sum);
+            solve(root,k,sum);
+        }
+    }
+    
+    int pathSum(TreeNode* root, int targetSum) {
+        
+        checkNode(root,targetSum,0);
+        return c;
+    }
+};
